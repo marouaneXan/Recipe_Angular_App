c9d47ac8e280f86ecd1b244eaa6fe58327550266
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-26 17:16:42 +0100

    generate a elements component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { CockpitComponent } from './databinding/cockpit/cockpit.component';
+import { ElementsComponent } from './databinding/elements/elements.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { CockpitComponent } from './databinding/cockpit/cockpit.component';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    CockpitComponent
+    CockpitComponent,
+    ElementsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/databinding/elements/elements.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements',
+  templateUrl: './elements.component.html',
+  styleUrls: ['./elements.component.css']
+})
+export class ElementsComponent {
+
+}
