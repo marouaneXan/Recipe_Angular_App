91a881fea25912454b4555508433550477d5b8d8
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-26 15:44:01 +0100

    add Recipe class type + recipe list + new recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[]=[
+    new Recipe('A test recipe','this is a simple recipe','')
+  ]
 
 }
